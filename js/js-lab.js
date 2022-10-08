77ba3ec65532b93e5815f9b653eb1d784239e7d8
Guo Tao <goto99@gmail.com>
2022-10-08 09:34:06 +0800

    updated js-lab and js-playground, etc.

--- js/js-lab.js
+++ js/js-lab.js
@@ -1,1 +1,16 @@
-document.write("Welcome from outer-js file!")
+document.write("Welcome from outer-js file!");document.write('<br>');
+
+document.write(5+null, '\n');document.write('<br>');
+
+document.write(typeof null);document.write('<br>');
+
+var m;
+document.write(typeof m);document.write('<br>');
+document.write(typeof m*2);document.write('<br>');
+
+var x, y='123', xy=true, oz=19.5;
+document.write(typeof x);document.write('<br>');
+
+function FirstTest(msg){
+	alert(msg)
+}

a187478a39da88a0f39e83ff23f2722cb3841497
Guo Tao <goto99@gmail.com>
2023-05-19 11:03:37 +0800

    Added several courses

---
+++ js/temp.js
@@ -0,0 +1,11 @@
+var score = 8;
+console.log("Mid-leve skills:", 0 < score < 10);
+
+var timeRemaining = 0;
+var energy = 10;
+console.log("Game over:", timeRemaining == 0 || energy == 0);
+
+var num1 = 2, num2 = 5, test1 = num1 % 2, test2 = num2 % 2;
+var result1 = (test1 == 0), result2 = (test2 == 0);
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2);
